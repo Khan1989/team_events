a8502bacba9aee6a638ebc6377d7e659d10dbe1d
Kevin <kevinhan@ie.ibm.com>
2017-11-29 17:15:15 +0000

    style event

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
-
+@Input() name;
+@Input() description;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleEvent(evt) {
+    console.log("toggling");
+    console.log(evt);
   }
 
 }
